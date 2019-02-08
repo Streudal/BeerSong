cc3315a2e27e61c6be039f7c89429e195b23f34e
streudal <tastreu@oakland.edu>
2019-02-08 16:42:42 -0500

    Created .gitignore file and BeerSong.java to initiate start of the assignment for the beer song.

---
+++ BeerSong.java
@@ -0,0 +1,15 @@
+package beersong;
+
+public class BeerSong {
+
+	public static void Ninety_Nine_Bottles_of_Beer(int bottles){        
+
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
