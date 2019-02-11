1bd03047ab3ad9ed120a991a252521a9ea646c64
streudal <tastreu@oakland.edu>
2019-02-11 15:36:50 -0500

    Added each case for when the bottles of beer get down to 0, 1, and the remaining bottles of beer on the wall. Also, the for loop initiates 99 bottles of beer then goes therough the entirety of the song.

--- BeerSong.java
+++ BeerSong.java
@@ -3,12 +3,28 @@ package beersong;
 public class BeerSong {
 
 	public static void Ninety_Nine_Bottles_of_Beer(int bottles){        
-
+		if(bottles == 0){
+			System.out.println();
+			System.out.println("No more bottles of beer on the wall, no bottles of beer.");
+			System.out.println("Go to the store and buy some more, " + (bottles + 99) + " bottles of beer on the wall.");
+		} else if( bottles == 1){
+			System.out.println(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer,take one");
+			System.out.println("down and pass it around, no more bottles of beer on the wall.");
+			System.out.println();
+		}
+		else {
+			System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer, take one");
+			System.out.println("down, and pass it around, " + (bottles -1) + " bottles of beer on the wall.");
+			System.out.println();
+		}
 
 	}
 
 	public static void main(String[] args) {
-
+		BeerSong beerSong = new BeerSong();
+		for (int i = 99; i>=0; i--){
+			beerSong.Ninety_Nine_Bottles_of_Beer(i);
+		}
 	}
 
 }
